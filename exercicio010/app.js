3fef5834643b1ce7d6aca70aec5102619ddd0a61
Henrincode <spellctro@gmail.com>
2025-09-28 13:42:49 -0300

    refatorando aula 10

--- exercicio010/app.js
+++ exercicio010/app.js
@@ -2,36 +2,58 @@ const botao = document.querySelector("button")
 
 botao.addEventListener("click", e => {
     e.preventDefault()
+    // Se dados forem enviados o botão fica bloqueado
     if (botao.classList.contains("btn-disabled")) return
 
-    const form = document.querySelector("form")
-    const resultado = document.querySelector("#resultado")
+    // variáveis para checar campos preenchidos
+    let nomeOK = true
+    let notasOK = true
 
-    const nome = form.fnome.value.trim()
-    const nota1 = parseFloat(form.fnota1.value)
-    const nota2 = parseFloat(form.fnota2.value)
-    const nota3 = parseFloat(form.fnota3.value)
-    const nota4 = parseFloat(form.fnota4.value)
+    // Seletores
+    const nome = document.querySelector("input[data-nome]")
+    const notas = Array.from(document.querySelectorAll("input[data-nota"))
 
-    const media = (nota1 + nota2 + nota3 + nota4) / 4
+    // Verifica se nome esta preenchido
+    if (nome.value.trim()) {
+        nome.classList.remove("preencher")
+    } else {
+        nome.classList.add("preencher")
+        nomeOK = false
+    }
 
+    // Verifica se todas as notas estão preenchidas
+    notas.forEach(nota => {
+        valor = nota.value
+
+        if (isNaN(valor) || !nota.value.trim()) {
+            nota.classList.add('preencher')
+            notasOK = false
+        } else {
+            nota.classList.remove('preencher')
+        }
+    })
+
+    // Se algum campo estiver faltando ele para aqui
+    if (!nomeOK || !notasOK) return
+
+    // Se os campos estiverem preenchidos desabilita botão e trata os dados
     botao.classList.add("btn-disabled")
 
-    const eLinha = elemento('div')
+    // Usa reduce para somar as notas e fazer a média
+    const media = notas.reduce((acc, nota) => acc + parseFloat(nota.value) || 0, 0) / notas.length
 
-    const eP = elemento('p')
-    eP.textContent = `A Média das notas de ${nome} é ${media}!`
+    // Cria paragrafo com resultado
+    const eP = document.createElement('p')
+    eP.textContent = `A Média das notas de ${nome.value.trim()} é ${media.toFixed(2)}!`
 
-    const eBotao = elemento("button")
+    // Cria botão para limpar inputs e fazer nova consulta
+    const eBotao = document.createElement('button')
     eBotao.textContent = "Fazer novo calculo"
     eBotao.addEventListener("click", e => {
         e.preventDefault()
 
-        form.fnome.value = ''
-        form.fnota1.value = ''
-        form.fnota2.value = ''
-        form.fnota3.value = ''
-        form.fnota4.value = ''
+        nome.value = ''
+        notas.forEach(nota => nota.value = '')
         resultado.innerHTML = ''
 
         botao.classList.remove("btn-disabled")
@@ -42,15 +64,3 @@ botao.addEventListener("click", e => {
 
 
 })
-
-
-function elemento(tipo, ...classes) {
-    const tag = document.createElement(tipo)
-    if (classes.length) {
-        console.log(tipo, ' tem classes ', classes)
-        tag.classList.add(...classes)
-    } else {
-        console.log(tipo, ' não tem classes')
-    }
-    return tag
-}
